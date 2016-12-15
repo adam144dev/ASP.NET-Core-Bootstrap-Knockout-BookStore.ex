7d37aebae86a4d84f564fdc41b61c5214fb1b492
adam144dev <adam144dev@gmail.com>
2016-12-15 11:45:06 +0100

    CartsSummary ViewController

--- Controllers/CartsController.cs
+++ Controllers/CartsController.cs
@@ -1,9 +1,13 @@
-﻿using ASP.NET_Core_Bootstrap_Knockout_BookStore.ex.Services;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using ASP.NET_Core_Bootstrap_Knockout_BookStore.ex.Models;
+using ASP.NET_Core_Bootstrap_Knockout_BookStore.ex.Services;
 using ASP.NET_Core_Bootstrap_Knockout_BookStore.ex.ViewModels;
-using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NET_Core_Bootstrap_Knockout_BookStore.ex.Controllers
 {
+    [ViewComponent(Name = "CartsSummary")]
     public class CartsController : Controller
     {
         private readonly ICartService _service;
@@ -13,22 +17,34 @@ namespace ASP.NET_Core_Bootstrap_Knockout_BookStore.ex.Controllers
             _service = service;
         }
 
-        //// GET: /<controller>/
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
-
-
-        // TBD: [ChildActionOnly]
-        public PartialViewResult Summary()
+        // GET: Carts
+        public ActionResult Index()
         {
-            // (TBD?) A unique identifier for the current session. This is not the same as the session cookie 
-            // since the cookie lifetime may not be the same as the session entry lifetime in the data store. 
             var cart = _service.GetBySessionId(HttpContext.Session.Id);
-            return PartialView(AutoMapper.Mapper.Map<CartViewModel>(cart));
+
+            return View(AutoMapper.Mapper.Map<Cart, CartViewModel>(cart));
         }
 
 
+        /// <summary>
+        // CartsSummary ViewController
+        /// </summary>
+
+        [ViewComponentContext]
+        public ViewComponentContext ViewComponentContext { get; set; }
+   
+        public IViewComponentResult Invoke()
+        {
+            // (TBD?) A unique identifier for the current session. This is not the same as the session cookie 
+            // since the cookie lifetime may not be the same as the session entry lifetime in the data store. 
+            var cart = _service.GetBySessionId(ViewComponentContext.ViewContext.HttpContext.Session.Id);
+            return new ViewViewComponentResult()
+            {
+                ViewData = new ViewDataDictionary<CartViewModel>(
+                    ViewData, 
+                    AutoMapper.Mapper.Map<Cart, CartViewModel>(cart)
+                )
+            };
+        }
     }
 }
